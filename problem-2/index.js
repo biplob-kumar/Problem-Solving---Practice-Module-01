2ed3929d2e0e4a53c57f6c094090102b2f75291a
biplob kumar <biplobkumar125@gmail.com>
2023-02-04 14:05:30 +0600

    first commit

---
+++ problem-2/index.js
@@ -0,0 +1,22 @@
+
+// . একটি জাভাস্ক্রিপ্ট প্রোগ্রাম লিখ যা যেকোনো ধনাত্মক সংখ্যা ইনপুট দিলে ১প্রিন্ট করবে,এবং শূন্য ইনপুট দিলে ০ প্রিন্ট করবে, অন্যতায় -১ প্রিন্ট করে।
+
+var i =prompt(" Enter your input");
+
+if(i>0 ){
+  document.write("its a  posivite number="+i ) ;
+    
+}
+else if (i==0){
+    document.write(" it,s a zero number = " +i);
+  }
+
+ else if(i<0) {
+    document.write(" it,s a negative number=" +i); 
+      
+  }
+  
+//    
+
+
+  
